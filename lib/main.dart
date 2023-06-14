a5d9d646c830839b9d1c6f399aaf4c17d16f2161
Muramsyah <ramdhanjr690@gmail.com>
2023-06-14 16:00:36 +0700

    core: add notification for android, ios, and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,115 @@
-import 'package:flutter/material.dart';
-import 'package:sample_code_isid/file_pick/file_pick_page.dart';
+import 'dart:developer';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:sample_code_isid/file_pick/file_pick_page.dart';
+import 'package:sample_code_isid/firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await setupFlutterNotifications();
+  showFlutterNotification(message);
+  print('Handling a background message ${message.messageId}');
+}
+
+late AndroidNotificationChannel channel;
+
+bool isFlutterLocalNotificationsInitialized = false;
+
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  isFlutterLocalNotificationsInitialized = true;
+}
+
+void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null && !kIsWeb) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          icon: 'launch_background',
+        ),
+      ),
+    );
+  }
+}
+
+late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+
+_checkNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  // use the returned token to send messages to users from your custom server
+  String? token = await messaging.getToken(
+    vapidKey:
+        "BKbBhLw9IXM05VyLmzB2PKWMDCPet5avjsM2TLGCQAtinAeccexDCHP4WRThv0rQ-Y3zpvgvbk6x7sQ0Hr36o9U",
+  );
+
+  log(token!);
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('User granted provisional permission');
+  } else {
+    print('User declined or has not accepted permission');
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  _checkNotificationPermission();
+
+  if (!kIsWeb) {
+    await setupFlutterNotifications();
+  }
+
   runApp(const MyApp());
 }
 
@@ -45,13 +153,6 @@ class HomePage extends StatelessWidget {
                   ),
                 ),
                 child: const Text('File Select'),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ElevatedButton(
-                onPressed: () {},
-                child: const Text('Push Notification'),
               ),
             ),
             Padding(
---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAKToaX_AGnXWi-VQGAleLBWL_znRm6gq4",
+    authDomain: "isid-push-notification-6dad3.firebaseapp.com",
+    projectId: "isid-push-notification-6dad3",
+    storageBucket: "isid-push-notification-6dad3.appspot.com",
+    messagingSenderId: "541073163713",
+    appId: "1:541073163713:web:583c37c4809f68934a9b40",
+    measurementId: "G-B278QMB1N9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Necessary to receive background messages:
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((m) => {
+    console.log("onBackgroundMessage", m);
+});
